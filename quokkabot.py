65e9e083687028aa8eeb2bfdd3b04215747a416a
j-c-s <jharris@fenwaygroup.com>
2018-03-02 09:42:28 -0600

    more tests

---
+++ quokkabot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import config
+
+bot = commands.Bot(command_prefix='¿', description=':D')
+
+@bot.event
+async def on_ready():
+    print('_,.-\'`¯¯`\'-.,_')
+    print('Logged in as:')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('¯`\'-.,__,.-\'`¯')
+
+@bot.command()
+async def hello():
+    await bot.say('Hello! :D')
+
+@bot.command()
+async def pls():
+    await bot.say('Yes? :D')
+
+@bot.command()
+async def say(args):
+    await bot.say(' '.join(args))
+
+bot.run(config.token)
